93a15eb7de64741dbbcf13ebff67bac3f7aefaa4
Justin Lindo <jlindo@masonlive.gmu.edu>
2021-02-17 14:00:11 -0500

    Update HelloServlet.java

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -33,7 +33,7 @@ public class HelloServlet extends HttpServlet
         out.println ("<BODY>");
         out.println ("<CENTER>");
 
-        out.println (" <B>Hello!</B><BR> <!--  English -->");
+        out.println (" <B>Hello! My name is Justin Lindo teehee</B><BR> <!--  English -->");
         out.println (" <B>Alo!</B><BR> <!--  Portuguese -->");
         out.println (" <B>Anyong haseyo!</B><BR> <!--  Korean -->");
         out.println (" <B>Apa Kabar!</B><BR> <!--  Indonesian -->");
