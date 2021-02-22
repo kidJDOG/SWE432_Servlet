462a18a1657b8f8de6af60ec4ca1b999bc1fc71b
Justin Lindo <jlindo@masonlive.gmu.edu>
2021-02-22 13:49:12 -0500

    Update EchoServlet.java
    Fixed error

--- src/main/java/servlet/EchoServlet.java
+++ src/main/java/servlet/EchoServlet.java
@@ -20,24 +20,24 @@ public class EchoServlet extends HttpServlet{
   @Override
    protected void doPost  (HttpServletRequest req, HttpServletResponse res)
    
-          throws ServletException, IOException{x
+	throws ServletException, IOException{
 
-     res.setContentType ("aplication/json");
-     res.setHeader("Access-Control-Allow-Origin", "*");
-     res.setHeader("Access-Control-Allow-Methods", "POST");
-     res.setHeader("Access-Control-Allow-Headers", "*");
+		res.setContentType ("aplication/json");
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Methods", "POST");
+		res.setHeader("Access-Control-Allow-Headers", "*");
 
-     PrintWriter out = res.getWriter();
+		PrintWriter out = res.getWriter();
 
-     Map<String, String[]> parameterMap = req.getParameterMap();
-     Map<String, String> data = new HashMap<String, String>();
-     for (String key: parameterMap.keySet()) {
-         String parameter = parameterMap.get(key)[0];
-         data.put(key, parameter);
-     }
-     out.print(new Gson().toJson(data));
-     out.flush();
-     out.close();
+		Map<String, String[]> parameterMap = req.getParameterMap();
+		Map<String, String> data = new HashMap<String, String>();
+		for (String key: parameterMap.keySet()) {
+		 String parameter = parameterMap.get(key)[0];
+		 data.put(key, parameter);
+		}
+		out.print(new Gson().toJson(data));
+		out.flush();
+		out.close();
     }
 
     @Override
