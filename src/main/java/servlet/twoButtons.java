b595a37be5b68cbea149fd034a35a76f4f6291c3
Justin Lindo <jlindo@masonlive.gmu.edu>
2021-02-23 18:57:24 -0500

    Update twoButtons.java
    idk at this point

--- src/main/java/servlet/twoButtons.java
+++ src/main/java/servlet/twoButtons.java
@@ -48,7 +48,7 @@ public class twoButtons extends HttpServlet
 
 // Location of servlet.
 static String Domain  = "https://swe432-justin-lindo.herokuapp.com/";
-static String Path    = "/twoButtons";
+static String Path    = "twoButtons";
 //static String Servlet = "twoButtons";
 
 // Button labels
