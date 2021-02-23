c0a97854720a604d2e4ca1f75c9e5d2ae71a5e6e
Justin Lindo <jlindo@masonlive.gmu.edu>
2021-02-23 18:54:18 -0500

    Update twoButtons.java
    fixed missing syntax

--- src/main/java/servlet/twoButtons.java
+++ src/main/java/servlet/twoButtons.java
@@ -130,7 +130,7 @@ private void PrintBody (PrintWriter out, String lhs, String rhs, String rslt)
    out.println("multiple submit buttons.");
    out.println("</p>");
    out.print  ("<form method=\"post\"");
-   out.println(" action=\"https://" + Domain + Path  "\">");
+   out.println(" action=\"https://" + Domain + Path  + "\">");
    out.println("");
    out.println(" <table>");
    out.println("  <tr>");
