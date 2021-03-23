e66695b13398baf6e084e35071fd44d46b270cc7
Justin Lindo <jlindo@masonlive.gmu.edu>
2021-03-23 19:00:41 -0400

    Update attributeServlet.java

--- src/main/java/servlet/attributeServlet.java
+++ src/main/java/servlet/attributeServlet.java
@@ -93,8 +93,8 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    {
       String att_name  = (String) e.nextElement();
       String att_value = (String) session.getAttribute(att_name);
-      String att_color = (String) e.nextElement();
-	  String att_value_color = (String) session.getAttribute(att_color);
+      String att_color = color;
+	  String att_value_color = color_value;
 
 
       out.print  ("<br><b>Name:</b> ");
